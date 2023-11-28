b7ccda6c25a964c219524ff15c094c45f3b06d40
florcita69nwn <150953035+florcita69nwn@users.noreply.github.com>
2023-11-28 13:41:13 -0300

    subiendo los trabajos

---
+++ pro-1/app.js
@@ -0,0 +1,52 @@
+//class 1
+// Este es un comentario de una linea
+/* comentario
+multilinea
+*/
+
+//las variables son un caja que almacenan un valor
+var minombre;
+
+//almacenar un valor en la variable creada
+minombre = "mateo";
+
+//ejercice 1
+// crear una variable, edad y guardarle un numero
+
+var edad;
+edad= 16;
+console.log(edad);
+
+//ejercice 2
+//inicializar 3 variables A, B y C con los valores
+//5, 10 y "I am a"
+
+
+// // cambia solo el codigo debajo de esta linea
+
+var a;
+var b; 
+var c;
+
+// // cambia solo el codigo encima de esta linea
+
+a = a + 1;
+b = b + 5;
+c = c + " string¡";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// class 2
+
+var nombre = prompt('ingrese su nombre')
+console.log('hola ,nombre')
+
+//class 3
+//ejercice 3
+// calcular el area y el perimetro de un cuarado
+
+let lado = 40;
+let area = lado * lado;
+let perimetro = lado *
